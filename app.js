f215a3f778fd4c4a0719696716f4c12cb0f40915
student00010671 <student00010671@gmail.com>
2021-04-01 18:21:14 -0400

    home page almost completed, styling was added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-. 
+const express = require('express')
+const app = express()
+
+// attach the pug as a template engine
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('./public'))
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => console.log("App is running on port 3000"))
