93b1e860b577e9f01f33788a1966dad8df9c3e6e
student00010671 <student00010671@gmail.com>
2021-04-04 05:22:06 -0400

    edit page was added

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/add', (req, res) => {
 
 app.post('/add', (req, res) => {
 	// get the sent data
-	const applicant = {
+	const employee = {
 		firstName: req.body.firstName,
 		lastName: req.body.lastName,
     email: req.body.email,
@@ -33,11 +33,12 @@ app.post('/add', (req, res) => {
 		wage: req.body.wage,
     comment: req.body.comment,
 		// I assume newly added employee won't start with vacation
-		group: "On Duty"
+		status: "On Duty"
 	} 
 
-	employeesRepo.add(applicant, (err) => {
-		if (err || applicant.inValidInput) {
+	employeesRepo.add(employee, (err) => {
+		// employee.invalidInput will be true only if employee's input is invalid. If it is valid invalidInput won't be created, hence won't be sent to the employees.json
+		if (err || employee.invalidInput) {
 			res.redirect('/add?error=true')
 		} else {
 			res.redirect('/add?success=true')
@@ -45,5 +46,49 @@ app.post('/add', (req, res) => {
 	})
 })
 
+app.get('/employees', (req, res) => {
+	const onDutyEmployees = employeesRepo.getAllOnDuty()
+	res.render('employees', {onDutyEmployees})
+})
+
+app.get('/terminated', (req, res) => {
+	const terminatedEmployees = employeesRepo.getAllTerminated()
+	res.render('terminated', {terminatedEmployees})
+})
+
+app.get('/employees/:id', (req, res) => {
+	const id = req.params.id
+	const employee = employeesRepo.getById(id)
+
+	res.render('employee', { employee })
+})
+
+app.get('/employees/:id/edit', (req, res) => {
+	const id = req.params.id
+	const employee = employeesRepo.getById(id)
+
+	res.render('edit', { employee, error: req.query.error })
+})
+
+app.post('/employees/:id/edit', (req, res) => {
+	const updatedEmployee = {
+		id: req.params.id,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+    email: req.body.email,
+    position: req.body.position,
+		wage: req.body.wage,
+    comment: req.body.comment,
+		status: req.body.status
+	}
+
+	employeesRepo.update(updatedEmployee.id, updatedEmployee, (err) => {
+		if (err || updatedEmployee.invalidInput) {
+			res.redirect('/employees/' + updatedEmployee.id + '/edit?error=true')
+		} else {
+			res.redirect('/employees/' + updatedEmployee.id)
+		}
+	})
+})
 
 app.listen(3000, () => console.log("App is running on port 3000"))
--- model/employees_repo.js
+++ model/employees_repo.js
@@ -18,14 +18,41 @@ class employeesRepository {
       this.employeesDb.push(employee)
       this.updateFile(callback)
     } else {
-      employee.inValidInput = true;
+      employee.invalidInput = true;
       callback()
     }
+  }
+
+  getAllOnDuty() {
+    return this.employeesDb.filter(employee => employee.status === 'On Duty')
+  }
+
+  getAllTerminated() {
+    return this.employeesDb.filter(employee => employee.status === 'Terminated')
   }
 
   // generating random ID to the employee
   generateRandomId() {
     return '_' + Math.random().toString(36).substr(2,9)
+  }
+
+  getById(id) {
+    return this.employeesDb.find(employee => employee.id === id)
+  }
+
+  update(id, updatedEmployee, callback) {
+    // find the index of the employee
+    const index = this.employeesDb.findIndex(employee => employee.id === id)
+
+    if(this.validInputs(updatedEmployee)){
+      // implement updates to the employee with the updates
+      this.employeesDb[index] = updatedEmployee
+      // update the whole json file
+      this.updateFile(callback)
+    } else {
+      updatedEmployee.invalidInput = true;
+      callback()
+    }
   }
 
   validInputs(employee){
